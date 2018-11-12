d44f997c3812c6fa5ac3a669dbc359d2d6c79c22
Anton <tosha37@inbox.ru>
2018-11-13 00:43:29 +0300

    13-11-18

---
+++ Matrix/TestingGraph/UserControls/ShowResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingGraph.UserControls
+{
+    public partial class ShowResult : UserControl
+    {
+        public ShowResult()
+        {
+            InitializeComponent();
+        }
+
+        private void setkaOK_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
